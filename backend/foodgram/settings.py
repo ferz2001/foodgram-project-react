03964954ee3aae8eee5b7b2ac868cb5db1004fa3
ferz2001 <fmannanov01@gmail.com>
2022-01-26 15:17:12 +0300

    last

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -55,7 +55,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_NAME', default='django.db.backends.postgresql'),
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
