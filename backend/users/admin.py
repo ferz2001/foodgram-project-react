79e8ef424566a804b93acc6658556a956533035f
ferz2001 <fmannanov01@gmail.com>
2022-01-31 20:39:21 +0300

    last

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,10 +1,5 @@
-from api.models import Follow, User
+from api.models import Follow
 from django.contrib import admin
 
 
-class UserAdmin(admin.ModelAdmin):
-    list_filter = ('username', 'email')
-
-
 admin.site.register(Follow)
-admin.site.register(User, UserAdmin)
