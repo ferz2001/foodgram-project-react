6ba502d62573c6477a34756b3ba36e4d56b8c22e
ferz2001 <fmannanov01@gmail.com>
2022-02-07 20:03:31 +0300

    last

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -54,7 +54,6 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
-    list_filter = ('name',)
     search_fields = ('name',)
 
 
